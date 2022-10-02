45e36d723ee0b1a6ae9ab3204026236b7c443e4d
balusoman <balucritaxzz@gmail.com>
2022-10-02 12:19:19 +0530

    structure added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./screens/home/home.module').then(m => m.HomeModule) }, { path: 'weather', loadChildren: () => import('./screens/weather/weather.module').then(m => m.WeatherModule) }];
+const routes: Routes = 
+[
+  { path: 'home', loadChildren: () => import('./screens/home/home.module').then(m => m.HomeModule) },
+  { path: '', loadChildren: () => import('./screens/weather/weather.module').then(m => m.WeatherModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/screens/weather/weather-routing.module.ts
+++ src/app/screens/weather/weather-routing.module.ts
@@ -2,7 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WeatherComponent } from './weather.component';
 
-const routes: Routes = [{ path: '', component: WeatherComponent }];
+const routes: Routes = [{ path: '', component: WeatherComponent ,
+children:[
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'dashboard'
+  },
+  { 
+    path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  { path: 'info', loadChildren: () => import('./info/info.module').then(m => m.InfoModule) }
+]
+},
+]
+
+
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
