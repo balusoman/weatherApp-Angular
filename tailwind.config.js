bfa8e00fcb9bbbc4dd7787c8f1174d308873f4bb
balusoman <balucritaxzz@gmail.com>
2022-10-02 11:31:37 +0530

    tailwindcss installed

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors:{
+        primaryOrange:'hsl(24, 100%, 50%)',
+        primaryBlack:'hsl(240, 40%, 5%)',
+      }
+    },
+  },
+  plugins: [],
+}
